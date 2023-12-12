fdc3929dfac3bd888443e99a8ce7cdb515e21b09
Richard Yuste <daw@richardyuste.com>
2023-12-12 11:12:37 +0100

    JAVA

--- JAVA/1ª EVALUACION/String__&&__format().java
+++ JAVA/1ª EVALUACION/String__&&__format().java
@@ -1,4 +1,4 @@
-//Métdos de String:
+//Métodos de String():
 	.length()						//Devuelve la cantidad de caracteres cadena.
 	.charAt(int i)					//Devuelve el carácter en la posición i (la primera posición es la 0).
 	.substring(int i, int j)		//Devuelve la subcadena desde el carácter en la posición i hasta la posición j. (Si no se pasa el parámetro j, se devuelve la subcadena hasta el final de la cadena)
@@ -22,9 +22,38 @@
 	saludo.replace('o', 'a')//Hala munda
 	saludo.split(" ")[0]	//Hola
 	saludo.split(" ")[1]	//mundo
-	
-	
-	
+
+
+//Métodos de StringBuilder():
+    .append()                 				//Añade texto al final.
+    .insert(int offset, …)					//Inserta texto en la posición especificada.
+    .delete(int start, int end)				//Elimina caracteres desde la posición start hasta end.
+    .deleteCharAt(int i)					//Elimina el carácter en la posición especificada.
+    .replace(int start, int end, String s)	//Reemplaza una subcadena por otra cadena.
+    .toString()								//Convierte el StringBuilder en un String.
+    .length()								//Devuelve la longitud del contenido.
+    .capacity()								//Devuelve la capacidad total.
+    .setLength(int newLength)				//Establece una nueva longitud.
+    .charAt(int i)							//Devuelve el carácter en la posición especificada.
+    .setCharAt(int i, char ch)				//Establece un carácter en la posición dada.
+    .substring(int start, int end)			//Devuelve una subcadena.
+
+	StringBuilder sb = new StringBuilder("Hola mundo");
+
+	sb.append("!!!")				//Hola mundo!!!
+	sb.insert(5, "planeta ")		//Hola planeta mundo!!!
+	sb.delete(5, 12)				//Hola mundo!!!
+	sb.deleteCharAt(4)				//Holamundo!!!
+	sb.replace(4, 9, " planeta")	//Hola planeta!!!
+	sb.toString()					//"Hola planeta!!!"
+	sb.length()						//15
+	sb.capacity()					//32 (depende de las operaciones realizadas)
+	sb.setLength(10)				//Hola plane
+	sb.charAt(3)					//a
+	sb.setCharAt(3, 'p')			//Holp plane
+	sb.substring(1, 5)				//olp 	
+
+
 //ESPECIFICADORES DE FORMATO:
 	d	//Número entero decimal.
 	f	//Número decimal con coma flotante.
