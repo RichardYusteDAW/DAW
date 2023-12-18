1031c436a8cd3487cfe1d87af609943b27389692
Richard Yuste <daw@richardyuste.com>
2023-12-18 21:17:49 +0100

    SQL

---
+++ DB/2ª EVALUACION/ejercicios/5.sql
@@ -0,0 +1,26 @@
+create database if not exists balneario;
+use balneario;
+create table balneario(
+	id_balneario smallint auto_increment primary key,
+	nombre varchar(60) not null
+);
+create table tratamiento(
+	id_tratamiento smallint auto_increment primary key,
+	nombre varchar(60) not null
+);
+
+create table terapeuta(
+	id_terapeuta smallint auto_increment primary key,
+    nombre varchar(60) not null
+);
+
+create table oferta(
+	id_balneario smallint not null,
+    id_tratamiento smallint not null,
+    id_terapeuta smallint not null,
+    coste decimal (6, 2) not null,
+    primary key (id_balneario, id_tratamiento),
+    foreign key (id_balneario) references balneario (id_balneario),
+    foreign key (id_tratamiento) references tratamiento (id_tratamiento),
+	foreign key (id_terapeuta) references terapeuta (id_terapeuta)
+);
--- DB/2ª EVALUACION/puebas.sql
+++
@@ -1,9 +0,0 @@
-show DATABASES;
-
-create DATABASE ciclismo;
-
-use ciclismo;
-
-show DATABASEs;
-
-CREATE Table ciciclista(nombre var(8), PRIMARY KEY);
