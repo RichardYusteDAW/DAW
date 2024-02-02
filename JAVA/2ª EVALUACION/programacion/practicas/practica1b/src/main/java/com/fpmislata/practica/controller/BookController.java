81075bbe2ed26031891c38f4b41c5b0934e80c19
Richard <info@wallacoin.io>
2024-02-02 13:51:52 +0100

    Java p2

--- JAVA/2ª EVALUACION/programacion/practicas/practica1b/src/main/java/com/fpmislata/practica/controller/BookController.java
+++ JAVA/2ª EVALUACION/programacion/practicas/practica1b/src/main/java/com/fpmislata/practica/controller/BookController.java
@@ -1,19 +1,28 @@
 package com.fpmislata.practica.controller;
 
-import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.fpmislata.practica.domain.entinty.Book;
 import com.fpmislata.practica.domain.service.BookService;
 import com.fpmislata.practica.domain.service.impl.BookServiceImpl;
 
+@RequestMapping("/books")
+@Controller
 public class BookController {
     BookService bookService = new BookServiceImpl();
 
-    public List<Book> findAll() {
-        return bookService.findAll();
+    @GetMapping
+    public String findAll(Model model) {
+        model.addAttribute("books", bookService.findAll());
+        return "books";
     }
 
-    public Book findById(int id) {
-        return bookService.findById(id);
+    @GetMapping("/{id}")
+    public String findById(@PathVariable("id") int id, Model model) {
+        model.addAttribute("book", bookService.findById(id));
+        return "bookDetails";
     }
 }
