8e9eb9b44950ddec71f8a48cced68e23f80b0ed5
Richard Yuste <daw@richardyuste.com>
2024-03-14 19:48:53 +0100

    gitignore explanation

--- gitPull.bat
+++ gitPull.bat
@@ -14,13 +14,23 @@ if "%hostname%"=="Win10" (
 )
 
 REM Hacer git pull en la carpeta DAW
+< NUL set /p ="DAW: "
 git -C "%daw%" pull
 
-REM Hacer git pull en la subcarpeta DAW\ED\Proyectos\YusteRichard-ED
+REM Hacer git pull en la subcarpeta DAW\WEBS\notes
+< NUL set /p ="Notes: "
+git -C "%daw%\WEBS\notes" pull
+
+REM Hacer git pull en la subcarpeta DAW\ED\Proyectos
+< NUL set /p ="YusteRichard-ED: "
 git -C "%daw%\ED\Proyectos\YusteRichard-ED" pull
 
-REM Hacer git pull en la subcarpeta DAW\ED\pruebas
+< NUL set /p ="Pruebas: "
 git -C "%daw%\ED\Proyectos\pruebas" pull
+
+< NUL set /p ="ASIX: "
+git -C "%daw%\ED\Proyectos\ASIX" pull
+
 
 endlocal
 
